bcbd05c0f574944daa9b6b4273c2c1ab80cc1346
razansinno <razanesinno54@gmail.com>
2025-10-05 02:31:07 +0300

    Initial commit

---
+++ assignment 5-10.py
@@ -0,0 +1,24 @@
+attempts = 3
+
+for i in range(attempts):
+    username = input("Enter your username: ")
+    password = int(input("Enter your password: "))
+
+    if username == "admin" and password == 1234:
+        print("✅ Login successful!")
+        n = int(input("Enter a number: "))
+        print(f"Prime numbers between 2 and {n} are:")
+        for num in range(2, n + 1):
+            for j in range(2, int(num ** 0.5) + 1):
+                if num % j == 0:
+                    break
+            else:
+                print(num)
+        break  # stop the loop after successful login
+
+    else:
+        remaining = attempts - (i + 1)
+        if remaining > 0:
+            print(f"Incorrect credentials. Attempts left: {remaining}\n")
+        else:
+            print("Account locked! Too many failed attempts.")
