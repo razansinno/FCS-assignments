59718816a25b6c2b43bd591fcfce7cb128adeba1
razansinno <razanesinno54@gmail.com>
2025-10-12 00:06:47 +0300

    New assignment

---
+++ Assignment 3.py
@@ -0,0 +1,27 @@
+print("Enter the number of students:")
+number = int(input())
+i=number
+students = []
+for i in range(number):
+    if number > 0:
+     student = number - (i + 1)
+     print(f"Enter student {i + 1} name:")
+     name = input()
+     print(f"Enter student {name} grade:")
+     grade = int(input())
+     students.append((name, grade))
+for name, grade in students: #I used AI here, because it was only printing the last student's name & grade
+     print(f"{name} - {grade}")
+
+for name, grade in students:
+ if grade < 60:
+   print(f"Failed Students: {name}")
+
+for name, grade in students:
+   max_grade = max([g for _, g in students])
+   if grade == max_grade:
+      print(f"Max Grade student is: {name},{grade}")
+
+
+
+    
